b08ef0ccde2272b5da46bc70c9586b3fc539169e
RikuKyl <t2kyri00@students.oamk.fi>
2026-02-18 12:44:07 +0200

    Versio 1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { CameraView, useCameraPermissions, BarcodeScanningResult } from 'expo-camera';
 
 export default function App() {
+  const [scanned, setScanned] = useState(false);
+  const [lastBarcode, setLastBarcode] = useState('');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) return <View />;
+  
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Tarvitsemme luvan kameran käyttöön</Text>
+        <Button onPress={requestPermission} title="Myönnä lupa" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }: BarcodeScanningResult) => {
+    setScanned(true);
+    setLastBarcode(data);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView
+        style={StyleSheet.absoluteFillObject}
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["ean13"],
+        }}
+      />
+      {scanned && (
+        <>
+        <Text style={styles.text}>Barcode: {lastBarcode}</Text>
+        <Button title={'Skannaa uudelleen'} onPress={() => setScanned(false)} />
+        </>
+      )}
     </View>
   );
 }
@@ -13,8 +45,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    marginBottom: 20,
+    textAlign: 'center'
+  }
 });
